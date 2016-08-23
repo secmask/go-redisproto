04924f6876f8bf15e3692489561c876e3f4be910
GiangTran <secmask@gmail.com>
2016-08-23 14:36:26 +0700

    update example

--- example/main.go
+++ example/main.go
@@ -1,97 +1,59 @@
-package main 
+package main
 
 import (
-	"github.com/secmask/go-redisproto"
-	"net"
-	"log"
 	"bufio"
+	"log"
+	"net"
 	"strings"
-	"sync"
+
+	"github.com/secmask/go-redisproto"
 )
-var (
-	mchan map[string] chan []byte
-	lock *sync.Mutex
-)
-func init(){
-	mchan = make(map[string] chan []byte)
-	lock = &sync.Mutex{}
-}
-func execLPush(qn string, data []byte){
-	lock.Lock()
-	defer lock.Unlock()
-	c,ok := mchan[qn]
-	if ok{
-		c <- data
-	}else{
-		c = make(chan []byte,1000000)
-		mchan[qn] = c
-		c <- data
-	}
-}
-func execRpop(qn string) []byte{
-	lock.Lock()
-	defer lock.Unlock()
-	c,ok := mchan[qn]
-	if ok{
-		select{
-			case msg:= <-c:
-				return msg
-			default:
-				return nil
-		}
-	}else{
-		return nil
-	}
-}
-func handleConnection(conn net.Conn){
+
+func handleConnection(conn net.Conn) {
 	defer conn.Close()
 	parser := redisproto.NewParser(conn)
-	w := bufio.NewWriter(conn)
+	writer := redisproto.NewWriter(bufio.NewWriter(conn))
 	var ew error
-	for{
-		command,err:=parser.ReadCommand()
-		if err!=nil{
-			_,ok:= err.(*redisproto.ProtocolError)
-			if ok{
-				ew = redisproto.SendError(w,err.Error())
-			}else{
-				log.Println(err, " closed connection to ",conn.RemoteAddr())
+	for {
+		command, err := parser.ReadCommand()
+		if err != nil {
+			_, ok := err.(*redisproto.ProtocolError)
+			if ok {
+				ew = writer.WriteError(err.Error())
+			} else {
+				log.Println(err, " closed connection to ", conn.RemoteAddr())
 				break
 			}
-		}else{
-			cmd:=strings.ToUpper(string(command.Get(0)))
-			switch cmd{
-				case "LPUSH":
-					qn:=string(command.Get(1))
-					execLPush(qn,command.Get(2))
-					ew = redisproto.SendInt(w,1)
-					break
-				case "RPOP":
-					qn:=string(command.Get(1))
-					data := execRpop(qn)
-					ew = redisproto.SendBulk(w,data)
-					break
-				default:
-					ew = redisproto.SendError(w,"Command not support")	
+		} else {
+			cmd := strings.ToUpper(string(command.Get(0)))
+			switch cmd {
+			case "GET":
+				ew = writer.WriteBulkString("dummy")
+			case "SET":
+				ew = writer.WriteBulkString("OK")
+			default:
+				ew = writer.WriteError("Command not support")
 			}
 		}
-		if ew!=nil{
-			log.Println("ew",ew)
+		if command.IsLast() {
+			writer.Flush()
+		}
+		if ew != nil {
+			log.Println("Connection closed", ew)
 			break
 		}
 	}
 }
 
-
-func main(){
-	listener,err:=net.Listen("tcp",":6380")
-	if err!=nil{
+func main() {
+	listener, err := net.Listen("tcp", ":6380")
+	if err != nil {
 		panic(err)
 	}
-	for{
-		conn,err:=listener.Accept()
-		if err!=nil{
-			log.Println("Error on accept: ",err)
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			log.Println("Error on accept: ", err)
 			continue
 		}
 		go handleConnection(conn)
